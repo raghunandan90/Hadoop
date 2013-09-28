431518d26488b6d45cf3843473c9d7041749f256
thiru_k <thirumala.kiran@gmail.com>
2013-09-28 23:35:14 +0530

    Initial commit

---
+++ 3. MapReduce/Java Code/EventTypeCount.java
@@ -0,0 +1,115 @@
+package com.hadoop.github;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/*
+ * Find total number of each event type
+ * 
+ */
+public class EventTypeCount {
+
+	// Map Class
+	public static class Map extends MapReduceBase implements
+			Mapper<LongWritable, Text, Text, IntWritable> {
+
+		private final JSONParser parser = new JSONParser();
+		private Text word = new Text();
+
+		/**
+		 * 
+		 */
+		@Override
+		public void map(LongWritable key, Text value,
+				OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+
+			JSONObject jsonObj = null;
+			// SPlit the input file to get individual jJSON document
+			String[] strs = value.toString().split("(?<=\\})(?=\\{)");
+
+			for (int i = 0; i < strs.length; i++) {
+
+				try {
+					jsonObj = (JSONObject) parser.parse(strs[i]);
+				} catch (ParseException e) {
+					e.printStackTrace();
+					System.out.println(strs[i]);
+					return;
+
+				}
+				Object eventType = jsonObj.get("type");
+				if (eventType != null) {
+					word.set(eventType.toString());
+					output.collect(word, new IntWritable(1));
+				}
+
+			}
+		}
+	}
+
+	// Reduce class
+	public static class Reduce extends MapReduceBase implements
+			Reducer<Text, IntWritable, Text, IntWritable> {
+
+		/**
+		 * 
+		 */
+		@Override
+		public void reduce(Text key, Iterator<IntWritable> values,
+				OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+			int sum = 0;
+			while (values.hasNext()) {
+				sum += values.next().get();
+			}
+
+			output.collect(key, new IntWritable(sum));
+		}
+	}
+
+	/**
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+
+		JobConf conf = new JobConf(EventTypeCount.class);
+		conf.setJobName("EventTypeCount");
+
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(IntWritable.class);
+
+		conf.setMapperClass(Map.class);
+		conf.setReducerClass(Reduce.class);
+
+		conf.setInputFormat(TextInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+		JobClient.runJob(conf);
+
+	}
+}
